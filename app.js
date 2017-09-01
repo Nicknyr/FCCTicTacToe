0db36ed1af7c16680b1c5be137c6346bdf1b16d4
Nick Kinlen <nick_kinlen@hotmail.com>
2017-08-31 23:03:53 -0400

    Basic game functionality working

--- app.js
+++ app.js
@@ -8,9 +8,55 @@ $(document).ready(function(){
     [' ', ' ', ' ']
   ];
 
-  function isGameOver (){
+  function isGameOver(){
+    //Check if horizontal winner
+    for(var i = 0; i < 3; i++){
+      if(grid[i][0] !== ' ' && grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2]){
+        return grid[i][0];
+      }
+    }
+    //Check if vertical winner
+    for(var j = 0; j < 3; j++){
+      if(grid[0][j] !== ' ' && grid[0][j] === grid[1][j] && grid[0][j] === grid[2][j]){
+        return grid[0][j];
+      }
+    }
 
-  };
+    // Check diagnoal top left to bottom right
+      if(grid[0][0] !== ' ' && grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]){
+        return grid[0][0];
+      }
+
+      // Check diagnoal top right to bottom left
+      if(grid[2][0] !== ' ' && grid[2][0] === grid[1][1] && grid[2][0] === grid[0][2]){
+        return grid[2][0];
+      }
+
+      //Check if all spaces on board are full
+      for(var i = 0; i < 3; i++){
+        for(var j = 0; j < 3; j++){
+          if([i][j] === ' '){
+            return false;
+          }
+        }
+      }
+
+      return null;
+  }
+
+  function moveAI(){
+    for(var i = 0; i < 3; i++){
+      for(var j = 0; j < 3; j++){
+        if(grid[i][j] === ' '){
+          return {
+          i: i,
+          j: j
+        };
+      }
+    }
+  }
+    return null;
+  }
 
   $('.col').click(function(){
     $this = $(this);
@@ -20,11 +66,29 @@ $(document).ready(function(){
     grid[i][j] = PLAYER_TOKEN;
     console.log(grid);
 
-    if(isGameOver()){
-
+    let gameState = isGameOver()
+    if(gameState){
+      alert('Game over' + gameState + ' wins');
+      return;
     } else {
       // It's the AI's turn
+      const move = moveAI();
+      grid[move.i][move.j] = COMPUTER_TOKEN;
+      $('.col[data-i=' + move.i + '][data-j=' + move.j + ']').html(COMPUTER_TOKEN);
+    }
 
+    gameState = isGameOver()
+    if(gameState){
+      alert('Game over' + gameState + ' wins');
+    }
+  });
+
+  $('#restart').click(function(){
+    for(var i = 0; i < 3; i++){
+      for(var j = 0; j < 3; j++){
+        grid[i][j] = ' ';
+        $('.col[data-i=' + i + '][data-j=' + j + ']').html(' ');
+      }
     }
   });
 
